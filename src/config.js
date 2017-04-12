9e135fddb8954609c714d0f636135f6f72d55293
Stepan <creadtive472@gmail.com>
2017-04-13 02:06:39 +1000

    OPTIONS chrome comment

--- src/config.js
+++ src/config.js
@@ -7,11 +7,18 @@
     /**
      * Настройки
      * Настройки расширения CHROME
-     * @return {OPTIONS} type Object 
+     * @return {
+            ID_EXPANSION    - Id расширения
+            STORAGE_NAME    - Id ключа базы в storage
+            REDIRECT_URL    - Адрес для редиректа блокированной страницы 
+            TYPE_OF_PAGES   - Тип блокирующий страницы
+            CONTEXT_MENUS   - Наименование в выпадающем меню
+            CONTEXTS_LI     - На каких элементах будет отображаться выпадающие меню    
+        } type Object 
     */
     
     return {
-        ID_EXPANSION: chrome.runtime.id,  //id расширения
+        ID_EXPANSION: chrome.runtime.id, 
         STORAGE_NAME: "block",
         REDIRECT_URL: "https://habrahabr.ru/",
         TYPE_OF_PAGES: "main_frame",
