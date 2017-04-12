deeeee514a2b5dc7904d37f8f445c4e35926c809
Stepan <creadtive472@gmail.com>
2017-04-13 00:50:42 +1000

    blockSite storage

--- src/app.js
+++ src/app.js
@@ -1,16 +1,60 @@
 (function(){
-   //chrome.webRequest.onHeadersReceived.addListener(function(details){
-   //     console.log(details);
-   //     return {redirectUrl: "https://habrahabr.ru/"};
-   // },{
-   //    types: [
-   //        'main_frame'
-   //    ],
-   //    urls: [
-   //        "*://www.google.ru/*",
-   //        "*://www.google.com/*"
-   //    ]
-   //},["blocking"])
+    
+     CHROMESTO.getStorage("block", function(block){
+         
+         var arr_block_url = [];
+         
+         for (key in block) {
+             arr_block_url[arr_block_url.length] = block[key]
+         }
+         
+         console.log(arr_block_url);
+         
+         if(arr_block_url.length === 0){
+             return;
+         }
+         
+         blockSite(arr_block_url);
+         
+    });
+    
+    function blockSite(arr_block){
+        chrome.webRequest.onHeadersReceived.addListener(function(details){
+            console.log(details);
+            return {redirectUrl: "https://habrahabr.ru/"};
+        },{
+            types: [
+                "main_frame"
+            ],
+            urls:arr_block
+        },["blocking"])
+    };
+    
+    
+    function storage(key, value){
+        CHROMESTO.getStorage("block", function(block){
+            
+            console.log(block);
+            
+            if(!block){
+                
+                var block = '{"block":{"'+key+'": "'+value+'"}}';
+                    block = JSON.parse(block);   
+   
+                CHROMESTO.setStorage(block, function(){
+                    chrome.runtime.reload();
+                });
+                return
+            }
+            
+            block[key] = value;
+            
+            CHROMESTO.setStorage({block: block}, function(){
+                chrome.runtime.reload();
+            });
+            
+        });
+    }
     
     
     chrome.contextMenus.create({
@@ -21,8 +65,15 @@
             "image","video","audio"
         ],
         "onclick": (function(info, tab){
-            console.log(tab);
+            
+            var tab_title = tab.title;
+            var url = tab.url;
+            
+            storage(tab_title, url)
+            
+            
+            //console.log([tab_title, url]);
         })
     });
     
-}())
+}());
--- src/content.js
+++ src/content.js
@@ -1,23 +1,22 @@
 (function(){
     
-    CHROMESTO.setStorage({
-        "history": {
-            "asd asd": "http://12",
-            "asds dd": "http://12s"
-        }
-    }, function(){});
-    
-    CHROMESTO.getStorage(null, function(page){
-        
-        var asd = page.history
-        
-        var keys = Object.keys(asd);
-        
-        var ssss = [];
-        
-        for (var i = 0, l = keys.length; i < l; i++) {
-            console.log(keys[i] + ' is ' + asd[keys[i]]);
-        }
-    });
+    //CHROMESTO.setStorage({
+    //    "block": {
+    //        "google": "http://google.com"
+    //    }
+    //}, function(){});
+    //
+    //CHROMESTO.getStorage(null, function(page){
+    //    
+    //    var asd = page.history
+    //    
+    //    var keys = Object.keys(asd);
+    //    
+    //    var ssss = [];
+    //    
+    //    for (var i = 0, l = keys.length; i < l; i++) {
+    //        console.log(keys[i] + ' is ' + asd[keys[i]]);
+    //    }
+    //});
     
 }())
