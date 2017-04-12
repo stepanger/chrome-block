87ba18c47ab4820f34b9248ea0571059db3e1a76
stepan <creadtive472@gmail.com>
2017-04-12 09:35:17 +0300

    chrome.commands.onCommand

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 (function(){
-   chrome.webRequest.onHeadersReceived.addListener(function(details){
-        console.log(details);
-        return {redirectUrl: "https://habrahabr.ru/"};
-    },{
-       types: ['main_frame'],
-       urls: ["*://www.google.ru/*"]
-   },["blocking"])
+   //chrome.webRequest.onHeadersReceived.addListener(function(details){
+   //     console.log(details);
+   //     return {redirectUrl: "https://habrahabr.ru/"};
+   // },{
+   //    types: [
+   //        'main_frame'
+   //    ],
+   //    urls: [
+   //        "*://www.google.ru/*",
+   //        "*://www.google.com/*"
+   //    ]
+   //},["blocking"])
+    
+    chrome.commands.onCommand.addListener(function(command) {
+        console.log('Command:', command);
+      });
+    
+    chrome.tabs.onUpdated.addListener(function(id,info,tab) {
+        console.log(tab);
+    });
+    
 }())
