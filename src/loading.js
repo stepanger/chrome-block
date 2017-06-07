d5287d4b8cedc438e46c5cd7a30901e37023590d
stepanger <creadtive472@gmail.com>
2017-06-07 18:44:31 +1000

    json load

--- src/loading.js
+++ src/loading.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://raw.githubusercontent.com/stepanger/Chocolate/storage/data/base.json", true);
+    console.log(xhr);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             console.log( xhr.responseText );
