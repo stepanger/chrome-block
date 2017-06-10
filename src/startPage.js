fa5bc073bfa22f7c3f5565ff6ae874632f9d01f0
stepanger <creadtive472@gmail.com>
2017-06-10 13:54:23 +1000

    notifications

--- src/startPage.js
+++ src/startPage.js
@@ -25,16 +25,26 @@
         
         console.log(details);
         
+        chrome.notifications.create("", {
+            type: "basic",
+            iconUrl: "icon/icon48.png",
+            title: "Chocolate",
+            message: "Вы заблокировали данный ресурс \n "+details.url,
+            eventTime: 3
+        },function(id){
+            timer = setTimeout(function(){
+                chrome.notifications.clear(id)
+            }, 3000);
+        })
+        
         return {
-            cancel: (function(){
-                return true
-            })
+            redirectUrl: OPTIONS.REDIRECT_URL
         };
     },{
     types: [
         OPTIONS.TYPE_OF_PAGES
     ],
     urls: Chocolate.blockWebRequest()
-    },["blocking"])
+    },["blocking"]);
     
 }());
